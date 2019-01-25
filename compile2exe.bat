fb84f98f5af681843b2d1f3f61fa3e9f0af1e4b5
Ben Kaufman <ben.whosgonna.com@gmail.com>
2019-01-25 16:03:24 -0600

    Trying to use new icon for complied application

--- compile2exe.bat
+++ compile2exe.bat
@@ -1,7 +1,8 @@
-set ver=0.0.2
+set ver=0.0.3
 
 pp -cd -M IO::All -M IO::File -M IO::All::Dir -B -x -c -o .\AltiLog2pcap.exe .\AltiLog2pcap.pl
-exe_update.bat --icon .\res\oldaltigen_icon.ico --info "CompanyName=AltiGen Communications, Inc.;FileVersion=%ver%;ProductVersion=%ver%" .\AltiLog2pcap.exe
+::exe_update.bat --icon .\res\oldaltigen_icon.ico --info "CompanyName=AltiGen Communications, Inc.;FileVersion=%ver%;ProductVersion=%ver%" .\AltiLog2pcap.exe
+exe_update.bat --icon .\res\newaltigen_icon.ico --info "CompanyName=AltiGen Communications, Inc.;FileVersion=%ver%;ProductVersion=%ver%" .\AltiLog2pcap.exe
 
 ::Comments        CompanyName     FileDescription FileVersion
 ::InternalName    LegalCopyright  LegalTrademarks OriginalFilename
